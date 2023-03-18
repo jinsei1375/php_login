54e3df15388d7490a5df198a5613015d79fa7ea0
jinsei1375 <murakami@kokochie.jp>
2023-03-18 17:42:06 +0900

    投稿一覧作成

--- src/index.php
+++ src/index.php
@@ -1,6 +1,38 @@
 <?php 
 
-  echo 'あああ';
+  $title = 'PHPログイン機能';
+  $path = './';
 
+  $header_path = $path . './parts/header.php';
 
+  include $header_path;
 ?>
+
+<div class="col-12 ml-3">
+  <table class="table table-striped">
+    <thead>
+      <tr>
+          <th>No</th>
+          <th>ユーザー名</th>
+          <th>投稿</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+          <td>1</td>
+          <td>エンジニア1</td>
+          <td>あああああああああああああああああああああああああああああああああ</td>
+      </tr>
+      <tr>
+          <td>2</td>
+          <td>エンジニア2</td>
+          <td>いいいいいいいいいいいいいいいい<br>ああああああああああああ</td>
+      </tr>
+      <tr>
+          <td>3</td>
+          <td>エンジニア3</td>
+          <td>VBA</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
