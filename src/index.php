3f19a9602241f0756e7270646619238cde0f7268
村上新聖 <murakami.shinsei.zn@gmail.com>
2023-07-01 11:14:14 +0900

    test

--- src/index.php
+++ src/index.php
@@ -29,7 +29,7 @@
               <a class="nav-link" href="/login.php">ログイン</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/register.php">登録テスト</a>
+              <a class="nav-link" href="/register.php">登録テストaaa</a>
             </li>
           </ul>
         </div>
